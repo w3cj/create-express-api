37bc5185f4a51cc258430d3cd9bd229f89abf541
w3cj <cj@null.computer>
2018-07-23 23:00:22 -0400

    :tada: initial commit :tada:

---
+++ index.js
@@ -0,0 +1,47 @@
+#! /usr/bin/env node
+
+const { spawn } = require('child_process');
+
+const name = process.argv[2];
+
+if (!name || !name.match(/^([a-zA-Z_-])+$/)) {
+  return console.log(`
+  Usage: create-express-api name-of-api  
+`);
+}
+
+const repoURL = 'https://github.com/w3cj/express-api-starter.git';
+
+runCommand('git', ['clone', repoURL, name])
+  .then(() => {
+    return runCommand('rm', ['-rf', `${name}/.git`]);
+  }).then(() => {
+    console.log('Installing dependencies...');
+    return runCommand('npm', ['install'], {
+      cwd: process.cwd() + '/' + name
+    });
+  }).then(() => {
+    console.log('Done! 🏁');
+    console.log('');
+    console.log('To get started:');
+    console.log('cd', name);
+    console.log('npm run dev');
+  });
+
+function runCommand(command, args, options = undefined) {
+  const spawned = spawn(command, args, options);
+
+  return new Promise((resolve) => {
+    spawned.stdout.on('data', (data) => {
+      console.log(data.toString());
+    });
+    
+    spawned.stderr.on('data', (data) => {
+      console.error(data.toString());
+    });
+    
+    spawned.on('close', () => {
+      resolve();
+    });
+  });
+}
