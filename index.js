4f4500d080114e7d268a19533f0314a8f7e4633c
Daniel Fimiani <50637582+danielfimiani@users.noreply.github.com>
2020-08-03 01:04:23 -0300

    Some changes to work also on Windows Shells
    Hey Cj , recently tried tu use this CLI that you made and ended having som troubles

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 #! /usr/bin/env node
 
 const { spawn } = require('child_process');
+//Add fs node module for deleting files.
+const fs = require('fs');
 
 const name = process.argv[2];
 if (!name || name.match(/[<>:"\/\\|?*\x00-\x1F]/)) {
@@ -11,16 +13,19 @@ if (!name || name.match(/[<>:"\/\\|?*\x00-\x1F]/)) {
 }
 
 const repoURL = 'https://github.com/w3cj/express-api-starter.git';
+const dir = `${name}/.git`;
 
 runCommand('git', ['clone', repoURL, name])
   .then(() => {
-    return runCommand('rm', ['-rf', `${name}/.git`]);
+    return rm(dir)
   }).then(() => {
     console.log('Installing dependencies...');
-    return runCommand('npm', ['install'], {
+    //Check if operative system is windows to pass npm.cmd or npm
+    return runCommand(/^win/.test(process.platform) ? 'npm.cmd' : 'npm', ['install'], {
       cwd: process.cwd() + '/' + name
     });
   }).then(() => {
+    console.log('');
     console.log('Done! 🏁');
     console.log('');
     console.log('To get started:');
@@ -29,7 +34,7 @@ runCommand('git', ['clone', repoURL, name])
   });
 
 function runCommand(command, args, options = undefined) {
-  const spawned = spawn(command, args, options);
+  const spawned = spawn( command, args, options);
 
   return new Promise((resolve) => {
     spawned.stdout.on('data', (data) => {
@@ -42,6 +47,21 @@ function runCommand(command, args, options = undefined) {
     
     spawned.on('close', () => {
       resolve();
+    });
+  });
+}
+
+//Replace rm command as a function to Work also on Windows (PowerShell/cmd)
+function rm (path){
+  return new Promise((resolve) =>{
+    fs.rmdir(path, { recursive: true }, (err) => {
+      if (err) {
+        console.error('Error removing file :', err);
+      }else {
+        //Optional , show or not message if folder deleted?
+        console.log(`${path} is deleted!`);
+        resolve();
+      }
     });
   });
 }
